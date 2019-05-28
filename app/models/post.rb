118e1ffc53044380bffd828831a532a73cc72bf2
Romeo Gono <romeogono1@aol.com>
2019-05-28 10:29:52 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,12 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validate :is_clickbait?
   
-  
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
   
   def is_clickbait?
     if CLICKBAIT_PATTERNS.none? { |pattern| pattern.match title }
